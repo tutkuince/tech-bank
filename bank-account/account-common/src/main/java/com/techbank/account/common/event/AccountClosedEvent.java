e9f3298b4ee87732eb98cd2da0d866063a44a6e4
Tutku Ince <tutku.ince@outlook.com>
2024-03-25 19:04:38 +0300

    update

--- bank-account/account-common/src/main/java/com/techbank/account/common/event/AccountClosedEvent.java
+++ bank-account/account-common/src/main/java/com/techbank/account/common/event/AccountClosedEvent.java
@@ -1,9 +1,7 @@
 package com.techbank.account.common.event;
 
 import com.techbank.cqrs.core.event.BaseEvent;
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 
 @Data
