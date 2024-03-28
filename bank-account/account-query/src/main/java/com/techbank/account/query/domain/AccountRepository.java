5892933c25b64a911b65290c273444f3d4cacf92
Tutku Ince <tutku.ince@outlook.com>
2024-03-28 17:56:19 +0300

    Domain Layer

---
+++ bank-account/account-query/src/main/java/com/techbank/account/query/domain/AccountRepository.java
@@ -0,0 +1,13 @@
+package com.techbank.account.query.domain;
+
+import com.techbank.cqrs.core.domain.BaseEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface AccountRepository extends JpaRepository<BankAccount, String> {
+    Optional<BankAccount> findByAccountHolder(String accountHolder);
+    List<BaseEntity> findByBalanceGreaterThan(double balance);
+    List<BaseEntity> findByBalanceLessThan(double balance);
+}
