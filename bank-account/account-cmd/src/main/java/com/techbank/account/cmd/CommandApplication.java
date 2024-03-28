55eb51f3f207450e91f071ae60755e7878f1fd78
Tutku Ince <tutku.ince@outlook.com>
2024-03-28 15:25:06 +0300

    Register Command Handlers

---
+++ bank-account/account-cmd/src/main/java/com/techbank/account/cmd/CommandApplication.java
@@ -0,0 +1,32 @@
+package com.techbank.account.cmd;
+
+import com.techbank.account.cmd.api.command.*;
+import com.techbank.cqrs.core.infrastructure.CommandDispatcher;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.annotation.PostConstruct;
+
+@SpringBootApplication
+public class CommandApplication {
+
+    private final CommandDispatcher commandDispatcher;
+    private final CommandHandler commandHandler;
+
+    public CommandApplication(CommandDispatcher commandDispatcher, CommandHandler commandHandler) {
+        this.commandDispatcher = commandDispatcher;
+        this.commandHandler = commandHandler;
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommandApplication.class, args);
+    }
+
+    @PostConstruct
+    public void registerHandlers() {
+        commandDispatcher.registerHandler(OpenAccountCommand.class, commandHandler::handle);
+        commandDispatcher.registerHandler(DepositFundsCommand.class, commandHandler::handle);
+        commandDispatcher.registerHandler(WithdrawFundsCommand.class, commandHandler::handle);
+        commandDispatcher.registerHandler(CloseAccountCommand.class, commandHandler::handle);
+    }
+}
