138cec710971a23c254fb638961e68ccef94f47d
Tutku Ince <tutku.ince@outlook.com>
2024-03-28 15:48:52 +0300

    EventProducer Interface and Implementation

---
+++ bank-account/account-cmd/src/main/java/com/techbank/account/cmd/infrastructure/AccountEventProducer.java
@@ -0,0 +1,21 @@
+package com.techbank.account.cmd.infrastructure;
+
+import com.techbank.cqrs.core.event.BaseEvent;
+import com.techbank.cqrs.core.producer.EventProducer;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AccountEventProducer implements EventProducer {
+
+    private final KafkaTemplate<String, Object> kafkaTemplate;
+
+    public AccountEventProducer(KafkaTemplate<String, Object> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
+    @Override
+    public void produce(String topic, BaseEvent event) {
+        kafkaTemplate.send(topic, event);
+    }
+}
