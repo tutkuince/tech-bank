e3209d85d748a158a2aca7678aa20d1c10186f96
Tutku Ince <tutku.ince@outlook.com>
2024-03-30 01:55:01 +0300

    Data Transfer Objects

---
+++ bank-account/account-cmd/src/main/java/com/techbank/account/cmd/api/dto/OpenAccountResponse.java
@@ -0,0 +1,20 @@
+package com.techbank.account.cmd.api.dto;
+
+import com.techbank.account.common.dto.BaseResponse;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@EqualsAndHashCode(callSuper = true)
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class OpenAccountResponse extends BaseResponse {
+    private String id;
+
+    public OpenAccountResponse(String message, String id) {
+        super(message);
+        this.id = id;
+    }
+}
