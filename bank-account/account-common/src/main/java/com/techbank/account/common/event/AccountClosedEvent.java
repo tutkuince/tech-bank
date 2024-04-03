b862ffae26897185deeb0ae7ca6ddff93bf80eb3
Tutku Ince <tutku.ince@outlook.com>
2024-04-03 23:47:15 +0300

    @NoArgsConstructor

--- bank-account/account-common/src/main/java/com/techbank/account/common/event/AccountClosedEvent.java
+++ bank-account/account-common/src/main/java/com/techbank/account/common/event/AccountClosedEvent.java
@@ -2,10 +2,12 @@ package com.techbank.account.common.event;
 
 import com.techbank.cqrs.core.event.BaseEvent;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 
 @Data
 @SuperBuilder
+@NoArgsConstructor
 public class AccountClosedEvent extends BaseEvent {
 
 }
